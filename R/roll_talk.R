b970ee8c503658a01cd70f302377c1972e28f6ac
Jason Foster <jason.j.foster@gmail.com>
2024-06-02 11:02:09 -0400

    Version: 1.0.0

---
+++ R/roll_talk.R
@@ -0,0 +1,19 @@
+##' Rolling and Expanding Statistics
+##'
+##' A presentation for rolling and expanding statistics of time-series data.
+##'
+##' @param ... Additional arguments passed to \code{\link[quarto::quarto_render]{quarto::quarto_render}}.
+##' @examples
+##' \dontrun{
+##' # rolling exploratory data analysis
+##' roll_talk()
+##' }
+##' @export
+roll_talk <- function(...) {
+
+  # https://github.com/quarto-dev/quarto-r/issues/81
+  input <- system.file("qmd", "roll_talk.qmd", package = "rolltalk")
+
+  quarto::quarto_render(input, ...)
+
+}
